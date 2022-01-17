b48a3790b288691c48105636f7fa827c6c6e0c59
Dhanush N <71968114+RorYin@users.noreply.github.com>
2022-01-17 20:50:11 +0530

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,11 +1,49 @@
-from PyQt5 import QtWidgets, uic
+import string
+import random
+from PyQt5 import QtWidgets, uic  
+
 import sys
-from PyQt5.QtGui import QIcon,QImage, QPixmap
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QLabel, QMessageBox
-import pyperclip
-from generator import genpassword
+import pyperclip #to copy from the output to the clipboard #pip install pyperclip
 
 
+#Code for generating strong password 
+lowercase=string.ascii_lowercase 
+uppercase=string.ascii_uppercase
+digits =string.digits
+symbols = string.punctuation
+
+def genpassword(l,h,d,s): 
+
+    password = ""
+    l=int(l)
+    h=int(h)
+    d=int(d)
+    s=int(s)
+
+    if l<1 or h<1 or d<1 or s<1:
+        return "1 is the minimum number for all fields"
+    try:
+
+        for i in range(0,h):
+            password=password+random.choice(uppercase)
+
+        for i in range(0,l):
+            password=password+random.choice(lowercase)
+
+        for i in range(0,d):
+            password=password+random.choice(digits)
+
+        for i in range(0,s):
+            password=password+random.choice(symbols)
+
+        return(password)
+    except:
+        return("Something went wrong")
+
+
+#Code for integrating gui components to python code
 
 class Screen1(QtWidgets.QMainWindow):
     def __init__(self):
@@ -28,7 +66,8 @@ class Screen1(QtWidgets.QMainWindow):
 
             print(self.l,self.u,self.s,self.d)
             
-            screen1.result.setText(genpassword(self.l,self.u,self.d,self.s))
+            result=genpassword(self.l,self.u,self.d,self.s)
+            screen1.result.setText(result)
         except:
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Warning)
@@ -47,6 +86,7 @@ widget.addWidget(screen1)
 
 widget.setWindowIcon(QIcon("uic/logo.png"))
 widget.setWindowTitle("password Generator GUI")
+
 widget.show()
 
 app.exec_()
